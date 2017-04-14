92378337c2a95a79fe89b080dcd570824b7cdea0
小朝 <13942156310@163.com>
2017-04-14 21:24:43 +0800

    test

---
+++ src/firsttest/test.java
@@ -0,0 +1,27 @@
+/**
+* <p>Title: test.java
+* <p>Description: 
+* @author 小朝
+* @date 2017年4月14日
+*/
+package firsttest;
+
+/**
+* <p>Title: test.java
+* <p>Description: 
+* @author 小朝
+* @date 2017年4月14日
+*/
+public class test {
+
+	/**
+	* <p>Title: main
+	* <p>Description: 
+	* @param args
+	*/
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		System.out.println("hello github");
+	}
+
+}
